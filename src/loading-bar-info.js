de3023eb5e9d1fad50e13e5d3ef90ec5de35e957
davidscheer58 <122395831+davidscheer58@users.noreply.github.com>
2023-04-23 22:26:57 -0400

    renovations

--- src/loading-bar-info.js
+++ src/loading-bar-info.js
@@ -1,105 +1,52 @@
 import{ LitElement, html, css} from 'lit'; 
 import "./loading-bar";
-import "./count-up.js"; 
-import { IntersectionObserverMixin } from "@lrnwebcomponents/intersection-element/lib/IntersectionObserverMixin.js"; 
+import '@lrnwebcomponents/count-up/count-up.js'; 
 
-class LoadingBarInfo extends IntersectionObserverMixin(LitElement) { 
+
+
+class LoadingBarInfo extends LitElement { 
     static properties = {
-        title: {type: String},
-        duration: {type: Number }, 
-        start: { type: Number}, 
-        end: {type: Number}, 
-        decimals: {type: Number}
+        bar: {type: Array},
 
     }
     static styles = css`
 
-    .wrapper{
-        text-align: center; 
-        margin-top: 20px; 
-        position: relative; 
-    }
-
-    .title-bar{
-        display: inline-block; 
-        color: #8e8e8e
-        min-width: 180px; 
-    }
-
-    .bar{
-        position: center;
-        margin-left: 50px; 
-
-    }
-
-    .timer{
-        position: aboslute; 
-        left: 79%; 
-        bottom: 36%; 
-
-    }
-
-    @media (max-width: 500px){ 
-        .wrapper {
-            transform: scale(0.7); 
-        }
-        .counter{
-            left: 88%; 
-        }
-    }
-    
-    @media (max-width: 500px) and (min-width: 500px) {
-        .wrapper { 
-            transform: scale(0.7); 
-        }
-        .counter{
-            left:84%; 
-        }
-    }
     
     `;
 
     constructor(){
         super(); 
-        this.title = 'Almost done'; 
-        this.duration = 10; 
-        this.start= 0; 
-        this.end = 10; 
-        this.decimals = 2; 
+      this.header = 'Final Project'; 
+      this.bar = []; 
+      this.createBars(); 
+
+    }
+    createBars(){
+        const address = new URL ('../assets/bar-api.js' , import.meta.url).href; 
+        fetch(address). then((response) => {
+            return response.json(); 
+            
+        }).then((data)=> {
+            this.bar= data; 
+        }); 
     }
     
     render(){
         return html`
-        <div class = "wrapper"> 
-            <div class= "title-bar">
-                <p>${this.title}</p>
-    </div> 
-    <loading-bar class= "bar" ></loading-bar> 
-    <count-up
-    class="counter"
-    duration=${this.duration} 
-    start=${this.start} 
-    end=${this.end} 
-    decimals=${this.decimals} 
-    noeasing=true>
-    </div> 
-        
+        <div> 
+
+            ${this.bar.map(bars => html`
+            <loading-bar title= "${bars.title}" endTime= "${bars.endTime}" startTime= "${bars.startTime}" widthSize="${this.widthSize}">
+    </loading-bar> 
+     <count-up></count-up> 
+     
+            
+            `)}
         `; 
 
     }
-updated(propertiesChanged) {
-    propertiesChanged.forEach((oldValue, propName) => {
-        if(propName == "elementVisible" && this[propName]) {
-            const bar = this.shadowRoot.querySelector('.bar').shadowRoot.querySelector(".loading-bar"); 
-            bar.style.width= "99.25%"; 
-            bar.style.transition = "width " + this.duration + "s linear"; 
-        }
-    });
 }
 
 
-
-
-}
 
 customElements.define('loading-bar-info', LoadingBarInfo); 
--- src/loading-bar.js
+++ src/loading-bar.js
@@ -1,49 +1,103 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css,unsafeCSS } from 'lit'; 
+import { IntersectionObserverMixin } from '@lrnwebcomponents/intersection-element/lib/IntersectionObserverMixin';
+import '@lrnwebcomponents/count-up/count-up.js'; 
 
 
 class LoadingBar extends LitElement {
-  static properties = {
+  static get properties() { 
+    let props= {}; 
+    if (super.properties) {
+      props = super.properties; 
+    }
+    return { 
+      startTime: {type: Numbrer },
+      endTime: {type: String} , 
+      title: {type: String},
+      widthSize: {type: Number} 
+    }
   }
-
   static styles = css`
   
+  
+  .bar-wrapper {
+      border: 1px solid #8e8e8e;  
+      justify-content: space-between; 
+      display: flex; 
+      display: inline-block; 
+    }
+  
 
-    .loading-bar {
-      width:0%; 
+
+
+    .styling {
+      width:40px; 
       height: 70px;
+      display: flex; 
+      padding: 5px; 
       border-radius: 20px; 
       margin: 3px; 
-      background: linear-gradient(to right, red, purple); 
-      transition: width 2s linear; 
-
       
 
     }
 
-    .bar-wrapper {
-      border: 1px solid #8e8e8e; 
-      border-radius: 20px; 
-      justify-content: space-between; 
-      display: flex; 
-      width: 50%; 
-      display: inline-block;
-      vertical-align: middle; 
+
+    
+
+    .text{
+      padding: 5px; 
+
+    }
+
+    .loading-bar-prop{
+      border-radius: 5px; 
+      background: linear-gradient(to right, red, yellow); 
+      height: 40px; 
+      width: 440px; 
+      animation-timing-function: linear; 
+      animation-fill-mode: normal; 
+      animation-delay: 0s; 
+      animation-duration: 5s; 
+      animation-name:fill; 
+
+
+    }
+
+    @keyframes fill{
+      0% {width: 0%;}
+      100%{width: 97.8;}
+
     }
 
     
   `;
 
   constructor() {
-    super();
+    super(); 
+    this.title = 'ist 256' 
+    this.startTime= 0; 
+    this.endTime = 20; 
+    this.widthSize = 40; 
+
 
   }
 
   render() {
     return html`
-     <div class="bar-wrapper">
-      <div class="loading-bar"></div> 
-      <div>
-    `;
+     ${this.elementVisible ? html`
+     <div class = 'wrapper'> 
+      <div class ="test" > 
+        ${this.title} 
+  </div> 
+  <div class= "barStyle" style= "width: ${this.widthSize}%">
+  <div class = "bar" style= "animation-duration: ${this.endtime}s; width: 100%; animation-delay: ${this.startTime}s"> 
+  </div> 
+  </div> 
+  <count-up start="${this.startTime}" end="${this.endTime}" duration= "${this.endTime}" noeasing= "true" decimalPlaces= "2" ></count-up>
+  </div> 
+  ` : ``};
+  `
+     
+     
   }
 }
 
